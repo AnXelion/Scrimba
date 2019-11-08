324d4ef55a6ba73fcdaab45eccf48fd9c25ed6d8
AnXelion <kongyei@gmail.com>
2019-11-08 14:45:21 +0800

    Finish Strings in JS.

--- introduction_to_javascript/index.js
+++ introduction_to_javascript/index.js
@@ -1,3 +1,9 @@
-const example = 'Dylan';
-example = 'Johnny';
-console.log(example);
+let firstName = 'Johnny';
+let lastName = 'Liu';
+
+console.log(`               ${firstName} ${lastName}`.length);
+console.log(`               ${firstName} ${lastName}`.trim().length);
+console.log(`${firstName} ${lastName}`.toUpperCase());
+console.log(`${firstName} ${lastName}`.toLowerCase());
+console.log(`${firstName} ${lastName}`.split(' '));
+console.log(`${firstName} ${lastName}`.split(''));
