cbdb81b77f2fa977b6275c7e6b7a0a0ce43a595d
AnXelion <kongyei@gmail.com>
2019-11-14 21:13:58 +0800

    Finish Arithmetic Operators in JavaScript.

--- introduction_to_javascript/index.js
+++ introduction_to_javascript/index.js
@@ -1,28 +1,3 @@
-let example1 = {
-    firstName: 'Johnny',
-    lastName: 'Liu',
-    address: {
-        city: 'Taipei',
-        district: 'unknown'
-    },
-    age: 27,
-    interest: ['guitar', 'video games']
-};
-
-example1.age = 28;
+let example1 = 5 % 6;
 
 console.log(example1);
-console.log(example1.hasOwnProperty('firstName'));
-console.log(example1.hasOwnProperty('surname'));
-
-
-let example2 = example1;    // pass by reference
-example2.abc = "abc";
-
-console.log(example1);
-console.log(example2);  
-
-let example3 = Object.assign({}, example1); // create a new object based on an exsiting one
-delete example3.firstName;
-console.log(example1);
-console.log(example3);
