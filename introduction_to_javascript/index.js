8eb3c87434fbf748549474f3680e1c14e8cea711
AnXelion <kongyei@gmail.com>
2019-11-14 22:17:42 +0800

    Finish Increment & Decrement.

--- introduction_to_javascript/index.js
+++ introduction_to_javascript/index.js
@@ -1,9 +1,14 @@
-let example1 = 5 === 5;     // true
-let example2 = 5 == '5';    // true
-let example3 = 6 != '6';    // false
-let example4 = 7 !== '7';   // true
+let example1 = 1;
+
+example1 = example1 + 1;
+example1 += 1;
+example1++;
+++example1;
+
+
+--example1;
+example1--;
+
+example1 %= 10;
 
 console.log(example1);
-console.log(example2);
-console.log(example3);
-console.log(example4);
