4bb3ee862f405c4741d6ef76990ef9824a140daf
AnXelion <kongyei@gmail.com>
2019-11-14 16:13:37 +0800

    Finish Arrays in JS.

--- introduction_to_javascript/index.js
+++ introduction_to_javascript/index.js
@@ -1,17 +1,10 @@
-let example1 = false;
-let example2 = true;
-let example3 = null;
-let example4 = undefined;
-let example5 = '';
-let example6 = NaN;
-let example7 = -5;
-let example8 = 0;
+let example1 = [5, 7, "a"];
 
-console.log(Boolean(example1));  // guess: false answer: false
-console.log(Boolean(example2));  // guess: true answer: true
-console.log(Boolean(example3));  // guess: false answer: false
-console.log(Boolean(example4));  // guess: false answer: false
-console.log(Boolean(example5));  // guess: true answer: false
-console.log(Boolean(example6));  // guess: false answer: false
-console.log(Boolean(example7));  // guess: true answer: true
-console.log(Boolean(example8));  // guess: false answer: false
+example1.push("B", "G");
+example1.pop();
+
+example1.forEach((element) => {
+    console.log(element + ' ' + typeof element);
+});
+
+console.log(example1);
