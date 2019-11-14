b87499c681d203a7b013fae7886d80582f3cc0a7
AnXelion <kongyei@gmail.com>
2019-11-14 19:49:17 +0800

    Finish Arrays in JavaScript (Challenge).

--- introduction_to_javascript/index.js
+++ introduction_to_javascript/index.js
@@ -7,4 +7,16 @@ example1.forEach((element) => {
     console.log(element + ' ' + typeof element);
 });
 
+
+let example2 = example1;    // pass by reference
+let example3 = [...example1];
+let example4 = example1.map((element) => {
+    return element;
+});
+
+example2.push("FUCK");
+
 console.log(example1);
+console.log(example2);
+console.log(example3);
+console.log(example4);
