96c531d16aeb2002fe65a89f198975b37de6c710
AnXelion <kongyei@gmail.com>
2019-11-14 21:10:59 +0800

    Finish Objects in JavaScript (Challenge).

--- introduction_to_javascript/index.js
+++ introduction_to_javascript/index.js
@@ -14,3 +14,15 @@ example1.age = 28;
 console.log(example1);
 console.log(example1.hasOwnProperty('firstName'));
 console.log(example1.hasOwnProperty('surname'));
+
+
+let example2 = example1;    // pass by reference
+example2.abc = "abc";
+
+console.log(example1);
+console.log(example2);  
+
+let example3 = Object.assign({}, example1); // create a new object based on an exsiting one
+delete example3.firstName;
+console.log(example1);
+console.log(example3);
