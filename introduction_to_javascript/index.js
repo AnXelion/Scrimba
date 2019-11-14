268652a27d565543922e9b031e60bf36e373661f
AnXelion <kongyei@gmail.com>
2019-11-14 15:52:28 +0800

    Finish Booleans in JS (Challenge).

--- introduction_to_javascript/index.js
+++ introduction_to_javascript/index.js
@@ -1,3 +1,17 @@
-let example1 = 566;
+let example1 = false;
+let example2 = true;
+let example3 = null;
+let example4 = undefined;
+let example5 = '';
+let example6 = NaN;
+let example7 = -5;
+let example8 = 0;
 
-console.log(Boolean(example1));
+console.log(Boolean(example1));  // guess: false answer: false
+console.log(Boolean(example2));  // guess: true answer: true
+console.log(Boolean(example3));  // guess: false answer: false
+console.log(Boolean(example4));  // guess: false answer: false
+console.log(Boolean(example5));  // guess: true answer: false
+console.log(Boolean(example6));  // guess: false answer: false
+console.log(Boolean(example7));  // guess: true answer: true
+console.log(Boolean(example8));  // guess: false answer: false
