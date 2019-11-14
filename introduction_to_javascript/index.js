b93bf09ecfb92ce668ecad94a113e949cdba7dec
AnXelion <kongyei@gmail.com>
2019-11-14 15:44:57 +0800

    Finish Booleans in JS.

--- introduction_to_javascript/index.js
+++ introduction_to_javascript/index.js
@@ -1,9 +1,3 @@
-let example1 = parseInt("Hello World 22");
-let example2 = parseFloat('44 Dylan 33');
-let example3 = 55.3333.toFixed(0);
-let example4 = 200.0.toFixed(2);
+let example1 = 566;
 
-console.log(typeof example1);   // null 
-console.log(typeof example2);   // number
-console.log(typeof example3);   // string
-console.log(typeof example4);   // string
+console.log(Boolean(example1));
