052ba737f4976dc2d402984a859492a39d448907
AnXelion <kongyei@gmail.com>
2019-11-14 15:42:47 +0800

    Finish Numbers in JS (Challenge).

--- introduction_to_javascript/index.js
+++ introduction_to_javascript/index.js
@@ -1,14 +1,9 @@
-// let number1 = 7;
-// let number2 = 7.5;
+let example1 = parseInt("Hello World 22");
+let example2 = parseFloat('44 Dylan 33');
+let example3 = 55.3333.toFixed(0);
+let example4 = 200.0.toFixed(2);
 
-// console.log(number1);
-// console.log(typeof number1);
-// console.log(number2);
-// console.log(typeof number2);
-
-
-let number = 6.6666;
-
-
-console.log(parseInt(number));
-console.log(number.toFixed(2));
+console.log(typeof example1);   // null 
+console.log(typeof example2);   // number
+console.log(typeof example3);   // string
+console.log(typeof example4);   // string
