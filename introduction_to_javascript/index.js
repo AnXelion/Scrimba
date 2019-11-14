6b8538dcaa430e977f3f57b1e1ca14a56f346815
AnXelion <kongyei@gmail.com>
2019-11-14 21:42:49 +0800

    Finish Relational Operators in JS (Challenge).

--- introduction_to_javascript/index.js
+++ introduction_to_javascript/index.js
@@ -1,10 +1,9 @@
-let example1 = 10;
-let example2 = 10;
-let example3 = '10';
+let example1 = 5 === 5;     // true
+let example2 = 5 == '5';    // true
+let example3 = 6 != '6';    // false
+let example4 = 7 !== '7';   // true
 
-console.log(example1 >= example2);
-console.log(example1 == example2);
-console.log(example1 != example2);
-console.log(example1 <= example2);
-console.log(example1 === example2); // true loose comparison (only value)
-console.log(example1 === example3); // false strict comparison (type & value)
+console.log(example1);
+console.log(example2);
+console.log(example3);
+console.log(example4);
