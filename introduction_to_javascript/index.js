a8991bef39c659298108d29cad623cfc786f5cd0
AnXelion <kongyei@gmail.com>
2019-11-14 21:29:05 +0800

    Finish Relational Operators in JavaScript.

--- introduction_to_javascript/index.js
+++ introduction_to_javascript/index.js
@@ -1,3 +1,10 @@
-let example1 = 5 % 6;
+let example1 = 10;
+let example2 = 10;
+let example3 = '10';
 
-console.log(example1);
+console.log(example1 >= example2);
+console.log(example1 == example2);
+console.log(example1 != example2);
+console.log(example1 <= example2);
+console.log(example1 === example2); // true loose comparison (only value)
+console.log(example1 === example3); // false strict comparison (type & value)
