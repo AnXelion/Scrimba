6199e8c97bb9eb459793b227bfc10aff734f832d
AnXelion <kongyei@gmail.com>
2019-11-15 21:37:35 +0800

    Finish includes()

--- introduction_to_es6+/index.js
+++ introduction_to_es6+/index.js
@@ -1,10 +1,3 @@
-function add(numArray = []) {
-    let total = 0;
-    numArray.forEach((element) => {
-        total =+ element;
-    });
-    
-    console.log(total);
-}
+let numArray = [1,2,3,4,5];
 
-add();
+console.log(numArray.includes(2))
