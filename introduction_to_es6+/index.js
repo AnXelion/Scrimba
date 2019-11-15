e8b396f069f92d10f9c00015214c97b810478076
AnXelion <kongyei@gmail.com>
2019-11-16 00:07:53 +0800

    Finish padStart() & padEnd()

--- introduction_to_es6+/index.js
+++ introduction_to_es6+/index.js
@@ -1,26 +1,4 @@
-// hoisting
-if (false) {
-    var example = 5;
-}
+let example = 'Dylan';
 
-console.log(example)
-
-/*
-var example;
-
-if (false) {
-    example = 5;
-}
-
-*/
-
-const example2 = 5;
-// example2 = 6;   // error
-console.log(example2);
-
-const example3 = [];
-example3.push(1);   // ok
-// example[0] = 5;  // error
-
-const example4 = {};
-example4.firstName = 'Johnny';
+console.log(example.padEnd(10, 'a'));
+console.log(example.padStart(10, 'b'));
