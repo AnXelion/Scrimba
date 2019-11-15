95308555aadf8079facf73158aa51bc79939ce5c
AnXelion <kongyei@gmail.com>
2019-11-15 14:50:45 +0800

    Finish Destructuring Objects

--- introduction_to_es6+/index.js
+++ introduction_to_es6+/index.js
@@ -1,21 +1,11 @@
-let word1 = 'Johnny';
-let word2 = 'Liu';
-let num1 = 5;
-let num2 = 6;
+const personalInformation = {
+    firstName: 'Dylan',
+    lastName: 'Israel',
+    city: 'Austin',
+    state: 'Texas',
+    zipCode: 73301
+};
 
-const fullName1 = word1 + ' ' + word2;
-const fullName2 = `${num1 + num2} ${word1} ${word2}`;
+const {firstName:  fn, lastName: ln} =  personalInformation;
 
-console.log(fullName1);
-console.log(fullName2);
-
-
-let example = 'Hello \n' + 'world!';
-console.log(example);
-document.getElementById('example').innerText = example;
-
-let example2 = `${word1} 
-${word2}
-`;
-console.log(example2);
-document.getElementById('example').innerText = example2;
+console.log(`${fn} ${ln}`);
