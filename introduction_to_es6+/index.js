fcd8f12964081e0a6220d064c6fa75c598b9a8e1
AnXelion <kongyei@gmail.com>
2019-11-15 22:33:12 +0800

    Finish Let & Const

--- introduction_to_es6+/index.js
+++ introduction_to_es6+/index.js
@@ -1,3 +1,26 @@
-let numArray = [1,2,3,4,5];
+// hoisting
+if (false) {
+    var example = 5;
+}
 
-console.log(numArray.includes(2))
+console.log(example)
+
+/*
+var example;
+
+if (false) {
+    example = 5;
+}
+
+*/
+
+const example2 = 5;
+// example2 = 6;   // error
+console.log(example2);
+
+const example3 = [];
+example3.push(1);   // ok
+// example[0] = 5;  // error
+
+const example4 = {};
+example4.firstName = 'Johnny';
