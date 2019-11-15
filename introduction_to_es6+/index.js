59e5061408068a4ce73db4bef6e8c6d3f1c9de26
AnXelion <kongyei@gmail.com>
2019-11-15 15:48:52 +0800

    Finish For of Loop (Challenge)

--- introduction_to_es6+/index.js
+++ introduction_to_es6+/index.js
@@ -1,6 +1,7 @@
-let fullName = "Dylan Coding God Israel";
+let incomes = [62000, 67000, 75000];
 
-
-for (const char of fullName) {
-    console.log(char);
+for (let income of incomes) {
+   income += 500;   // no effect
 }
+
+console.log(incomes);
